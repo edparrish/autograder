fdfb584f65094bd2d55cc9fdf2ae46fc9c394a21
edparrish <edparris@cabrillo.edu>
2017-06-29 18:28:44 -0700

    Add files via upload

---
+++ test/testfiles/cpp2/hello.cpp
@@ -0,0 +1,17 @@
+/**
+    CS-11 Asn 0, hello.cpp
+    Purpose: Prints a message to the screen.
+
+    @author Ed Parrish
+    @version 1.0 6/14/17
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter your name: ";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name <<"!\n";
+    return 0;
+} // end of main function
