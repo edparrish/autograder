d30dd1d0d93270e262b172ad136e0e481b6a7978
Ed Parrish <edparris@cabrillo.edu>
2017-07-09 13:28:20 -0700

    Add files via upload

---
+++ includes/dbconvars-template.php
@@ -0,0 +1,8 @@
+<?php
+// Database connection variables.
+// Rename this file to dbconvars.php and fill in the following variables.
+$dbhost = "localhost";
+$dbuser = "put db user name here";
+$dbpwd = "put db user password here";
+$dbname = "put database name here";
+?>
