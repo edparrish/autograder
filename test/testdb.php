74503d3d0a013c9966ebf64d815db6768cde16b6
Ed Parrish <edparris@cabrillo.edu>
2017-07-10 13:08:09 -0700

    Add files via upload

--- test/testdb.php
+++ test/testdb.php
@@ -9,85 +9,62 @@ $students = null; // for all students
 $students = array("db1", "db2"); // list student folders to test
 
 class GradeRunner extends Grader {
-    // Setup test environment to run before any grading.
-    function startTest() {
-        parent::startTest();
-        echo "Running startTest\n";
-    }
-
     // Test commands to run for each student submission.
     function test() {
-        // Database query tests
-        $globs = array("[qQ]*1.*", "[qQ]*2.*", "[qQ]*3.*", "[qQ]*4.*");
-        $ff = new Filefinder($globs);
-        $path = dirname($ff->findLargestFile());
-        $this->pass(new TestCondition(substr_count($path, " ") > 0), -1,
-            "Do not put spaces in folder names like you did with \"$path\"");
-        $fcList = FileContents::toFileContents($ff);
-        $this->pass(new TestMatchAny("/\brtf/", $fcList), -1,
-            "Please submit plain text and NOT RTF files", $fcList);
+        // Load database
+        $ff = new FileFinder("*.sql");
+        $dbFile = $ff->findLargestFile();
+        $this->run(new TestLoadDB($dbFile));
+        $eval = new LoadDBEvaluator(4);
+        $score = $this->report($eval, "Database Implementation Score:");
 
-        // Query 1 credit for any attempt
-        $this->fail(new TestFileExists("[qQ]*1.*"), -2,
-            "No query1.txt file");
-        // Query 2 credit for any attempt
-        $this->fail(new TestFileExists("[qQ]*2.*"), -2,
-            "No query2.txt file");
-        // Query 3 find most likely file
-        $ff = new FileFinder("[qQ]*3.*");
-        $ff->filterName("/\.log/i");
-        $ff->filterName("/\.7z/i");
-        $ff->filterName("/\.zip/i");
-        $ff->filterName("/\.rar/i");
+        // Test query 1
+        $ff = new FileFinder("[qQ]*1.txt");
         $sqlFile = $ff->findFirstFile();
-        $this->fail(new TestCondition($sqlFile), -2, "No query3.txt file");
+        $this->fail(new TestCondition($sqlFile), -2, "No query1.txt file");
         if ($sqlFile) {
             $queryFC = new FileContents($sqlFile);
             $queryFC->trimWhitespace();
             $studentSql = $queryFC->toString();
-            $this->fail(new TestCondition($studentSql), -2,
-                "Empty query3.txt file", $sqlFile);
-            $this->run(new TestRunLogSQL($studentSql, "query3.log",
-                "artzy"), $studentSql);
+            $tc = new TestCondition($studentSql);
+            $this->fail($tc, -2, "Empty query1.txt file");
+            $tc = new TestRunLogSQL($studentSql, "query1.log", "test");
+            $this->run($tc, $studentSql);
             $sql = "SELECT ProductID, PriceEach, Quantity
                     FROM orderitems
                     WHERE OrderID = 2";
             // Compare against required output
-            $this->fail(new TestCompareSQL($sql, $studentSql, "artzy"),
-                -1, "Error in query 3", $studentSql);
+            $tc = new TestCompareSQL($sql, $studentSql, "test");
+            $this->fail($tc, -1, "Error in query 1", $studentSql);
         }
-        // Query 4 find most likely file
-        $ff = new FileFinder("[qQ]*4.*");
-        $ff->filterName("/\.log/i");
-        $ff->filterName("/\.7z/i");
-        $ff->filterName("/\.zip/i");
-        $ff->filterName("/\.rar/i");
+        // Test query 2
+        $ff = new FileFinder("[qQ]*2.txt");
         $sqlFile = $ff->findFirstFile();
-        $this->fail(new TestCondition($sqlFile), -2, "No query4.txt file");
+        $this->fail(new TestCondition($sqlFile), -2, "No query2.txt file");
         if ($sqlFile) {
             $queryFC = new FileContents($sqlFile);
             $queryFC->trimWhitespace();
             $studentSql = $queryFC->toString();
-            $this->fail(new TestCondition($studentSql), -2,
-                "Empty query4.txt file", $sqlFile);
-            $this->run(new TestRunLogSQL($studentSql, "query4.log",
-                "artzy"), $studentSql);
+            $tc = new TestCondition($studentSql);
+            $this->fail($tc, -2, "Empty query2.txt file");
+            $tc = new TestRunLogSQL($studentSql, "query2.log", "test");
+            $this->run($tc, $studentSql);
             $sql = "SELECT SupplierName,purchorders.Qty,DateOrdered,ProductName
                 FROM suppliers, products, purchorders
                 WHERE suppliers.ID=products.SupplierID
                 AND products.ID=purchorders.ProductID
                 ORDER BY SupplierName, DateOrdered";
             // Compare against required output
-            $pass = $this->fail(new TestCompareSQL($sql, $studentSql, "artzy"),
-                -1, "Error in query 4", $studentSql);
+            $tc = new TestCompareSQL($sql, $studentSql, "test");
+            $this->fail($tc, -1, "Error in query 2", $studentSql);
             // Check query for required elements.
             $pat = "/ORDER\s+BY\s+(suppliers\.)?SupplierName/i";
             $this->fail(new TestMatch($pat, $queryFC), 0,
-                "-Query 4 missing clause: ORDER BY SupplierName", !$pass);
+                "-Query 2 missing clause: ORDER BY SupplierName", !$pass);
         }
         // Evaluate the results
-        $score = $this->report(new ValueEvaluator(8), "SQL Queries Score:");
-        $this->writeGradeLog(" -Well done!\n", 8 == $score);
+        $score = $this->report(new ValueEvaluator(4), "SQL Queries Score:");
+        $this->writeGradeLog(" -Well done!\n", 4 == $score);
 
         // README.txt
         $this->run(new TestReadme($this));
@@ -99,21 +76,18 @@ class GradeRunner extends Grader {
         $maxScore = $this->getMaxScore();
         $this->writeGradeLog("Subtotal Score: $subtotal of $maxScore\n");
 
-        // Extra credit query 5
-        $ff = new FileFinder("[qQ]*5.*");
-        $ff->filterName("/\.7z/i");
-        $ff->filterName("/\.zip/i");
-        $ff->filterName("/\.rar/i");
+        // Extra credit query 3
+        $ff = new FileFinder("[qQ]*3.txt");
         $sqlFile = $ff->findFirstFile();
         $this->passFail(new TestCondition($sqlFile),
             1, "Submitted extra-credit query",
-            0, "Did not turn in extra credit file: query5.txt");
+            0, "Did not turn in extra credit file: query3.txt");
         if ($sqlFile) {
             $queryFC = new FileContents($sqlFile);
             $queryFC->trimWhitespace();
             $studentSql = $queryFC->toString();
             $this->fail(new TestCondition($studentSql), -1,
-                "Empty query5.txt file", $sqlFile);
+                "Empty query3.txt file", $sqlFile);
             $this->run(new TestRunLogSQL($studentSql, "query5.log",
                 "artzy"), $studentSql);
             $sql = "SELECT SupplierName,suppliers.ID,ProductName,products.ID
@@ -131,19 +105,12 @@ class GradeRunner extends Grader {
             100=>"Excellent work!",
             90=>"Overall, excellent work!",
             80=>"Overall good work with a few missing pieces.",
-            70=>"Overall satisfactory work with some problems and missing pieces.",
-            60=>"Overall passable work with several problems and missing pieces",
-            50=>"Overall you had some errors and missing work that affected your score.",
-            20=>"You are missing the main part of the assignment.",
+            70=>"Overall satisfactory with some problems and missing pieces.",
+            60=>"Overall passable with some problems and missing pieces",
+            50=>"Overall has errors and missing work that affected the score.",
             0=>"You are missing most parts of the assignment"
         );
         $this->reportOverall($maxScore, false, $comments);
-    }
-
-    // Tear down test environment after all tests
-    function finishTest() {
-        parent::finishTest();
-        echo "Running finishTest\n";
     }
 }
 $grader = new GradeRunner(TEST_DIR, $students);
