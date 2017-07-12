8a2dcd51678f8e42d83399466e6a2e0971a360f0
Ed Parrish <edparris@cabrillo.edu>
2017-07-11 21:38:13 -0700

    Add files via upload

--- test/testsystem.php
+++ test/testsystem.php
@@ -241,7 +241,6 @@ class GradeRunner extends Grader {
     function testCodeLab() {
         $this->setSectionName("testCodeLab");
         echo "...testing TestCodeLab\n";
-        flush();
         $firstName = $this->getFirstName();
         $lastName = $this->getLastName();
         $this->run(new TestCodeLab(CODELAB_TABLE, $lastName, $firstName));
