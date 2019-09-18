cf245523c4b00b2cb955986129c836d14ac69eef
Ed Parrish <edparris@cabrillo.edu>
2019-09-18 13:41:48 -0700

    Renamed parameters and vars
    Renamed parameters and vars

--- testcase.php
+++ testcase.php
@@ -564,11 +564,15 @@ class TestCompileJava extends TestCase {
         // Compile files
         fwrite($handle, "Command: $this->cmd\n");
         if (strtoupper(substr(php_uname('s'), 0, 3)) === 'WIN') {
-            $errout = ERROUT;  // errout collects info for Windows
-            $info = `$errout $this->cmd`;
+//            $errout = ERROUT;  // errout collects info for Windows
+//            $info = `$errout $this->cmd`;
+//echo "cmd=";var_dump("$this->cmd >1 out.log 2>&1");
+            $info = `$this->cmd 1> out.msg 2>&1`;
         } else {
             $info = `$this->cmd 2>&1`;
         }
+        $info = file_get_contents('out.msg');
+echo "info=";var_dump($info);
         $info = trim($info);
         fwrite($handle, $info."\n");
         if (substr_count($info, "Usage: javac") != 0) {
@@ -718,6 +722,10 @@ class TestFileExists extends TestCase {
             } else {
                 $filesFound = glob($glob, GLOB_BRACE);
             }
+            // Following for odd chars in file name like [ ] added 9/10/2019
+            if (!$filesFound && file_exists($glob)) {
+                $filesFound = array($glob);
+            }
             if ($filesFound) {
                 $fileList = array_merge($fileList, $filesFound);
             }
@@ -731,7 +739,7 @@ class TestFileExists extends TestCase {
 }
 /**
     Tests if all of a list of patterns exists in every file meeting $glob.
- */
+*/
 class TestMatch extends TestCase {
     var $patList;
     var $fileList;
